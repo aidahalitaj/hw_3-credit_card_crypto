5740a70670f77deffedd2bcd6376049f72d245fb
aidahalitaj <aida.halitaj@uni-pr.edu>
2018-03-16 00:23:27 +0800

    Adding gems

--- credit_card.rb
+++ credit_card.rb
@@ -1,45 +1,81 @@
 require_relative './luhn_validator.rb'
 require 'json'
 
+require_relative './luhn_validator.rb'
+require 'json'
+
+# Credit Card Class
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
-    # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
-      # TODO: setup the hash with all instance vairables to serialize into json
+      'number' => @number,
+      'expiration_date' => @expiration_date,
+      'owner' => @owner,
+      'credit_network' => @credit_network,
     }.to_json
   end
 
-  # returns all card information as single string
-  def to_s
-    to_json
-  end
+  class CreditCard
+    # TODO: mixin the LuhnValidator using an 'include' statement
+    include LuhnValidator
+    # instance variables with automatic getter/setter methods
+    attr_accessor :number, :expiration_date, :owner, :credit_network
 
-  # return a new CreditCard object given a serialized (JSON) representation
-  def self.from_s(card_s)
-    # TODO: deserializing a CreditCard object
-  end
+    def initialize(number, expiration_date, owner, credit_network)
+      # TODO: initialize the instance variables listed above
+      @number = number
+      @expiration_date = expiration_date
+      @owner = owner
+      @credit_network = credit_network
+      @hash_attr = {}
+    end
 
-  # return a hash of the serialized credit card object
-  def hash
-    # TODO: implement this method
-    #   - Produce a hash (using default hash method) of the credit card's
-    #     serialized contents.
-    #   - Credit cards with identical information should produce the same hash
-  end
+    # returns json string
+    def to_json
+      {
+        # TODO: setup the hash with all instance variables to serialize into json
+        number: @number, expiration_date: @expiration_date,
+        owner: @owner, credit_network: @credit_network
+      }.to_json
+    end
 
-  # return a cryptographically secure hash
-  def hash_secure
-    # TODO: implement this method
-    #   - Use sha256 from openssl to create a cryptographically secure hash.
-    #   - Credit cards with identical information should produce the same hash
+    # returns all card information as single string
+    def to_s
+      to_json
+    end
+
+    # return a new CreditCard object given a serialized (JSON) representation
+    def self.from_s(card_s)
+      # TODO: deserializing a CreditCard object
+    end
+
+    # return a hash of the serialized credit card object
+    def hash
+      # TODO: implement this method
+      #   - Produce a hash (using default hash method) of the credit card's
+      #     serialized contents.
+      #   - Credit cards with identical information should produce the same hash
+      @hash_attr = to_s
+    end
+
+    # return a cryptographically secure hash
+    def hash_secure
+      # TODO: implement this method
+      #   - Use sha256 from openssl to create a cryptographically secure hash.
+      #   - Credit cards with identical information should produce the same hash
+    end
   end
 end
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,11 +1,21 @@
+# Module for algorithm Luhn Validator
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
-
+    sum_odd = sum_double = 0
     # TODO: use the integers in nums_a to validate its last check digit
+    nums_a = number.to_s.chars.map(&:to_i)
+    nums_a.reverse.each_slice(2) do |odd, even|
+      # remove odd numbers from reversed array and add them to sum_odd
+      sum_odd += odd.to_i
+
+      double = even.to_i * 2
+      double -= 9 if double >= 10
+      sum_double += double
+    end
+    ((sum_odd + sum_double) % 10).zero?
   end
 end
